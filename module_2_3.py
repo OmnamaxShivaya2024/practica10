4e143c6c02f095baeb3875d65064aad4bbd1e5af
Людмила Кузнецова <ananim2023@list.ru>
2024-08-13 11:33:51 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,16 @@
+my_list = [42, 69, 322, 13, 0, 99, - 5, 9, 8, 7, - 6, 5]  # исходный список
+index = 0
+while index<len(my_list):
+    num= my_list[index]
+    index=index+1
+    if num == 0:
+        continue
+    elif num < 0:
+        break  # прерываем цикл, если число отрицательное
+    print(num)
+
+
+
+
+
+
